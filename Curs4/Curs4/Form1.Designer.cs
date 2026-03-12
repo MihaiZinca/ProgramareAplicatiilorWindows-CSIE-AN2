c4e2c3002a6a7796fac9ddb6deba0247e3beadad
MihaiZinca <zincamihai24@stud.ase.ro>
2026-03-12 20:12:36 +0200

    Add files via upload

---
+++ Curs4/Curs4/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Curs4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.el = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pel = new System.Windows.Forms.TextBox();
+            this.lb = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // el
+            // 
+            this.el.Location = new System.Drawing.Point(71, 52);
+            this.el.Name = "el";
+            this.el.Size = new System.Drawing.Size(316, 26);
+            this.el.TabIndex = 0;
+            this.el.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.el.TextChanged += new System.EventHandler(this.el_TextChanged);
+            this.el.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.el_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(316, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Apasa pe EL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pel
+            // 
+            this.pel.Location = new System.Drawing.Point(71, 186);
+            this.pel.Name = "pel";
+            this.pel.ReadOnly = true;
+            this.pel.Size = new System.Drawing.Size(316, 26);
+            this.pel.TabIndex = 2;
+            this.pel.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lb
+            // 
+            this.lb.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb.FormattingEnabled = true;
+            this.lb.ItemHeight = 23;
+            this.lb.Location = new System.Drawing.Point(71, 245);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(316, 119);
+            this.lb.TabIndex = 3;
+            this.lb.SelectedIndexChanged += new System.EventHandler(this.lb_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 454);
+            this.Controls.Add(this.lb);
+            this.Controls.Add(this.pel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.el);
+            this.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox el;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox pel;
+        private System.Windows.Forms.ListBox lb;
+    }
+}
+
---
+++ Curs4/Curs4/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//font Consolas are aceiasi matrice de pixeli.. sa putem sa asezam textul frumos aliniat
+namespace Curs4
+{
+    public partial class Form1 : Form
+    {
+        Element obel;
+        public Form1()
+        {
+            InitializeComponent();
+            obel = new Element();
+            obel.eveniment_modificare_valoare += Obel_eveniment_modificare_valoare;
+        }
+
+        private void Obel_eveniment_modificare_valoare(Element obj)
+        {
+            pel.Text=obj.Patrat.ToString();
+            //construiesc sirul
+            string s = $"{obj.Valoare,6} | {obj.Patrat,8}|"; //obj.valoare,6 -retine val in 6 caractere
+            lb.Items.Add(s);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            el.Text=obel.Valoare.ToString();
+            pel.Text=obel.Patrat.ToString();
+        }
+
+        private void el_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                obel.Valoare = int.Parse(el.Text); //schimba starea valoarea si ridica evenimentul
+                lb.SelectedIndex = -1;
+           
+
+
+        }
+
+        private void el_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //evenimentul keypress Retunr=Enter , adica verifica cand apesi enter
+            if (e.KeyChar == (char)Keys.Return)//in momentul in care s-a apasat return incepe sa faca ceva
+            {
+                button1_Click(null, null);
+                el.SelectAll();
+            }
+        }
+
+        private void lb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lb.SelectedIndex != -1)
+            {
+                string sel = lb.SelectedItem as string; //pt conversie echivalent ca (cast)
+                el.Text = sel.Split('|')[0].Trim();//vreau element[0] nu patratul
+            }
+        }
+    }
+}
