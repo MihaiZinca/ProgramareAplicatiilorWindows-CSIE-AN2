1c2c907e5f5ba53c6314c8039b520b3db6c1c08d
MihaiZinca <zincamihai24@stud.ase.ro>
2026-03-31 12:49:28 +0300

    Add files via upload

---
+++ Seminar6/Seminar6/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Seminar6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lvStudenti = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tvStudenti = new System.Windows.Forms.TreeView();
+            this.tbStudenti = new System.Windows.Forms.TextBox();
+            this.btnPreluareLV = new System.Windows.Forms.Button();
+            this.btnStergeElemLV = new System.Windows.Forms.Button();
+            this.btnPopulareTV = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.stergeElemtLVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.stergeElementTVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvStudenti
+            // 
+            this.lvStudenti.CheckBoxes = true;
+            this.lvStudenti.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvStudenti.GridLines = true;
+            this.lvStudenti.HideSelection = false;
+            this.lvStudenti.Location = new System.Drawing.Point(11, 61);
+            this.lvStudenti.Name = "lvStudenti";
+            this.lvStudenti.Size = new System.Drawing.Size(305, 235);
+            this.lvStudenti.TabIndex = 0;
+            this.lvStudenti.UseCompatibleStateImageBehavior = false;
+            this.lvStudenti.View = System.Windows.Forms.View.Details;
+            this.lvStudenti.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.lvStudenti_ItemChecked);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Matricol";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nume";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Medie";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Observatie";
+            // 
+            // tvStudenti
+            // 
+            this.tvStudenti.Location = new System.Drawing.Point(352, 61);
+            this.tvStudenti.Name = "tvStudenti";
+            this.tvStudenti.Size = new System.Drawing.Size(276, 235);
+            this.tvStudenti.TabIndex = 1;
+            this.tvStudenti.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvStudenti_AfterSelect);
+            // 
+            // tbStudenti
+            // 
+            this.tbStudenti.Location = new System.Drawing.Point(696, 70);
+            this.tbStudenti.Multiline = true;
+            this.tbStudenti.Name = "tbStudenti";
+            this.tbStudenti.ReadOnly = true;
+            this.tbStudenti.Size = new System.Drawing.Size(280, 226);
+            this.tbStudenti.TabIndex = 2;
+            // 
+            // btnPreluareLV
+            // 
+            this.btnPreluareLV.Location = new System.Drawing.Point(25, 348);
+            this.btnPreluareLV.Name = "btnPreluareLV";
+            this.btnPreluareLV.Size = new System.Drawing.Size(108, 53);
+            this.btnPreluareLV.TabIndex = 3;
+            this.btnPreluareLV.Text = "PopulareLV";
+            this.btnPreluareLV.UseVisualStyleBackColor = true;
+            this.btnPreluareLV.Click += new System.EventHandler(this.btnPreluareLV_Click);
+            // 
+            // btnStergeElemLV
+            // 
+            this.btnStergeElemLV.Location = new System.Drawing.Point(199, 348);
+            this.btnStergeElemLV.Name = "btnStergeElemLV";
+            this.btnStergeElemLV.Size = new System.Drawing.Size(103, 53);
+            this.btnStergeElemLV.TabIndex = 4;
+            this.btnStergeElemLV.Text = "Stergere element LV";
+            this.btnStergeElemLV.UseVisualStyleBackColor = true;
+            this.btnStergeElemLV.Click += new System.EventHandler(this.btnStergeElemLV_Click);
+            // 
+            // btnPopulareTV
+            // 
+            this.btnPopulareTV.Location = new System.Drawing.Point(330, 348);
+            this.btnPopulareTV.Name = "btnPopulareTV";
+            this.btnPopulareTV.Size = new System.Drawing.Size(118, 53);
+            this.btnPopulareTV.TabIndex = 5;
+            this.btnPopulareTV.Text = "Populare TV";
+            this.btnPopulareTV.UseVisualStyleBackColor = true;
+            this.btnPopulareTV.Click += new System.EventHandler(this.btnPopulareTV_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stergeElemtLVToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(207, 36);
+            // 
+            // stergeElemtLVToolStripMenuItem
+            // 
+            this.stergeElemtLVToolStripMenuItem.Name = "stergeElemtLVToolStripMenuItem";
+            this.stergeElemtLVToolStripMenuItem.Size = new System.Drawing.Size(206, 32);
+            this.stergeElemtLVToolStripMenuItem.Text = "Sterge Elemt LV";
+            this.stergeElemtLVToolStripMenuItem.Click += new System.EventHandler(this.stergeElemtLVToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stergeElementTVToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(241, 69);
+            // 
+            // stergeElementTVToolStripMenuItem
+            // 
+            this.stergeElementTVToolStripMenuItem.Name = "stergeElementTVToolStripMenuItem";
+            this.stergeElementTVToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.stergeElementTVToolStripMenuItem.Text = "Sterge element TV";
+            this.stergeElementTVToolStripMenuItem.Click += new System.EventHandler(this.stergeElementTVToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1284, 599);
+            this.Controls.Add(this.btnPopulareTV);
+            this.Controls.Add(this.btnStergeElemLV);
+            this.Controls.Add(this.btnPreluareLV);
+            this.Controls.Add(this.tbStudenti);
+            this.Controls.Add(this.tvStudenti);
+            this.Controls.Add(this.lvStudenti);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvStudenti;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.TreeView tvStudenti;
+        private System.Windows.Forms.TextBox tbStudenti;
+        private System.Windows.Forms.Button btnPreluareLV;
+        private System.Windows.Forms.Button btnStergeElemLV;
+        private System.Windows.Forms.Button btnPopulareTV;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem stergeElemtLVToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem stergeElementTVToolStripMenuItem;
+    }
+}
+
---
+++ Seminar6/Seminar6/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seminar6
+{
+    public partial class Form1 : Form
+    {
+        List<Student> listaStudenti = new List<Student>();
+        public Form1()
+        {
+            InitializeComponent();
+            incarcareDate();
+            lvStudenti.ContextMenuStrip = contextMenuStrip1;
+            lvStudenti.ContextMenuStrip = contextMenuStrip2;
+        }
+
+        private void incarcareDate()
+        {
+            StreamReader sr = new StreamReader("Studenti.txt");
+            string linie;
+            while((linie=sr.ReadLine())!=null) //citire linie cu linie din fisier
+            {
+                try
+                {
+                    int matricol = Convert.ToInt32(linie.Split(',')[0]);
+                    string nume = linie.Split(',')[1];
+                    float medie = float.Parse(linie.Split(',')[2],
+                        CultureInfo.InvariantCulture.NumberFormat); 
+                    //pt a putea programul sa citeasac punctul de la medie(conversie in float)
+
+                    Student s = new Student(matricol, nume, medie);
+                    listaStudenti.Add(s);
+                }catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                } 
+            }
+            sr.Close();
+            MessageBox.Show("Datele au fost incarcate!");
+        }
+
+        private void btnPreluareLV_Click(object sender, EventArgs e)
+        {
+
+            foreach(Student s in listaStudenti)
+            {
+                ListViewItem item = new ListViewItem(s.matricol.ToString());
+                item.SubItems.Add(s.nume);
+                item.SubItems.Add(s.medie.ToString());
+
+                if (s.medie > 9) //populare ult coloanei din LV cu un text
+                    item.SubItems.Add("Excelent");
+                else
+                    item.SubItems.Add("Bun");
+
+                lvStudenti.Items.Add(item);
+            }
+        }
+
+        private void btnStergeElemLV_Click(object sender, EventArgs e)
+        {
+            foreach(ListViewItem itm in lvStudenti.Items)
+            {
+                if (itm.Checked) itm.Remove();
+            }
+        }
+
+        private void stergeElemtLVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem itm in lvStudenti.Items)
+            {
+                if (itm.Checked) itm.Remove();
+            }
+        }
+
+        private void lvStudenti_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+            if (e.Item.Checked)
+                e.Item.BackColor = Color.Red;
+            // pentru a pune culoarea roșu pe fundalul unui rând din ListView,
+            // atunci când acesta este selectat.
+        }
+
+        private void btnPopulareTV_Click(object sender, EventArgs e)
+        {
+            TreeNode parinte = new TreeNode("Studenti");
+            //se creaza nodul parinte(prin cls TreeNode) dupa se atribuie tvStudenti
+            tvStudenti.Nodes.Add(parinte);
+            
+            foreach(Student s in listaStudenti)
+            {
+                TreeNode copil = new TreeNode(s.matricol + "-" + s.nume + "-" + s.medie);
+                parinte.Nodes.Add(copil);
+
+                //pt fiecare stud din lista se creeaza un nod copil cu date despre student,
+                //iar apoi se adauga la nodul parinte(se real leg dintre cele 2 noduri)
+
+                TreeNode nepot = new TreeNode();
+
+                if (s.medie > 9)
+                    nepot.Text = "Excelent";
+                else
+                    nepot.Text = "Bun";
+
+                copil.Nodes.Add(nepot);
+            }
+            tvStudenti.ExpandAll();
+        }
+
+        private void tvStudenti_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode nodSelectat = tvStudenti.SelectedNode;
+            int matricol;
+            try
+            {
+                matricol = Convert.ToInt32(nodSelectat.Text.Split('-')[0]);
+
+            }
+            catch
+            {
+                matricol = 0;
+            }
+
+            foreach(Student s in listaStudenti)
+            {
+                if (s.matricol == matricol)
+                    tbStudenti.Text += s.ToString() + Environment.NewLine;
+            }
+        }
+
+        private void stergeElementTVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TreeNode nodSelectat = tvStudenti.SelectedNode;
+            if(nodSelectat.NextNode!=null)
+            {
+                nodSelectat = tvStudenti.SelectedNode.NextNode;
+            }
+            else
+            {
+                if (nodSelectat.PrevNode != null)
+                    nodSelectat = tvStudenti.SelectedNode.PrevNode;
+            }
+
+            tvStudenti.SelectedNode.Remove();
+            tvStudenti.SelectedNode = nodSelectat;
+        }
+    }
+}
