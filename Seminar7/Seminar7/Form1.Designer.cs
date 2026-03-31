c092ba302bd12f3e2ce5d9aea588f35d69c3c26a
MihaiZinca <zincamihai24@stud.ase.ro>
2026-03-31 13:49:08 +0300

    Add files via upload

---
+++ Seminar7/Seminar7/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Seminar7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.tbDenumire = new System.Windows.Forms.TextBox();
+            this.tbPret = new System.Windows.Forms.TextBox();
+            this.tbCantitate = new System.Windows.Forms.TextBox();
+            this.tbValoare = new System.Windows.Forms.TextBox();
+            this.lvProduse = new System.Windows.Forms.ListView();
+            this.btnAdaugaLinie = new System.Windows.Forms.Button();
+            this.btnCalculeazaTotal = new System.Windows.Forms.Button();
+            this.tbValoareTotala = new System.Windows.Forms.TextBox();
+            this.btnComanda = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 37);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(281, 26);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // tbDenumire
+            // 
+            this.tbDenumire.Location = new System.Drawing.Point(12, 93);
+            this.tbDenumire.Name = "tbDenumire";
+            this.tbDenumire.Size = new System.Drawing.Size(106, 26);
+            this.tbDenumire.TabIndex = 1;
+            // 
+            // tbPret
+            // 
+            this.tbPret.Location = new System.Drawing.Point(136, 93);
+            this.tbPret.Name = "tbPret";
+            this.tbPret.Size = new System.Drawing.Size(106, 26);
+            this.tbPret.TabIndex = 2;
+            this.tbPret.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbCifre_KeyPress);
+            // 
+            // tbCantitate
+            // 
+            this.tbCantitate.Location = new System.Drawing.Point(261, 93);
+            this.tbCantitate.Name = "tbCantitate";
+            this.tbCantitate.Size = new System.Drawing.Size(106, 26);
+            this.tbCantitate.TabIndex = 3;
+            this.tbCantitate.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbCifre_KeyPress);
+            // 
+            // tbValoare
+            // 
+            this.tbValoare.Location = new System.Drawing.Point(393, 93);
+            this.tbValoare.Name = "tbValoare";
+            this.tbValoare.ReadOnly = true;
+            this.tbValoare.Size = new System.Drawing.Size(106, 26);
+            this.tbValoare.TabIndex = 4;
+            // 
+            // lvProduse
+            // 
+            this.lvProduse.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvProduse.GridLines = true;
+            this.lvProduse.HideSelection = false;
+            this.lvProduse.Location = new System.Drawing.Point(523, 37);
+            this.lvProduse.Name = "lvProduse";
+            this.lvProduse.Size = new System.Drawing.Size(368, 190);
+            this.lvProduse.TabIndex = 5;
+            this.lvProduse.UseCompatibleStateImageBehavior = false;
+            this.lvProduse.View = System.Windows.Forms.View.Details;
+            // 
+            // btnAdaugaLinie
+            // 
+            this.btnAdaugaLinie.Location = new System.Drawing.Point(523, 269);
+            this.btnAdaugaLinie.Name = "btnAdaugaLinie";
+            this.btnAdaugaLinie.Size = new System.Drawing.Size(133, 55);
+            this.btnAdaugaLinie.TabIndex = 6;
+            this.btnAdaugaLinie.Text = "Adauga Linie";
+            this.btnAdaugaLinie.UseVisualStyleBackColor = true;
+            this.btnAdaugaLinie.Click += new System.EventHandler(this.btnAdaugaLinie_Click);
+            // 
+            // btnCalculeazaTotal
+            // 
+            this.btnCalculeazaTotal.Location = new System.Drawing.Point(758, 269);
+            this.btnCalculeazaTotal.Name = "btnCalculeazaTotal";
+            this.btnCalculeazaTotal.Size = new System.Drawing.Size(133, 55);
+            this.btnCalculeazaTotal.TabIndex = 7;
+            this.btnCalculeazaTotal.Text = "Calculeaza Total";
+            this.btnCalculeazaTotal.UseVisualStyleBackColor = true;
+            this.btnCalculeazaTotal.Click += new System.EventHandler(this.btnCalculeazaTotal_Click);
+            // 
+            // tbValoareTotala
+            // 
+            this.tbValoareTotala.Location = new System.Drawing.Point(523, 375);
+            this.tbValoareTotala.Multiline = true;
+            this.tbValoareTotala.Name = "tbValoareTotala";
+            this.tbValoareTotala.ReadOnly = true;
+            this.tbValoareTotala.Size = new System.Drawing.Size(133, 38);
+            this.tbValoareTotala.TabIndex = 8;
+            // 
+            // btnComanda
+            // 
+            this.btnComanda.Location = new System.Drawing.Point(758, 377);
+            this.btnComanda.Name = "btnComanda";
+            this.btnComanda.Size = new System.Drawing.Size(130, 36);
+            this.btnComanda.TabIndex = 9;
+            this.btnComanda.Text = "Comanda";
+            this.btnComanda.UseVisualStyleBackColor = true;
+            this.btnComanda.Click += new System.EventHandler(this.btnComanda_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Denumire";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Pret";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Cantitate";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Valoare";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 520);
+            this.Controls.Add(this.btnComanda);
+            this.Controls.Add(this.tbValoareTotala);
+            this.Controls.Add(this.btnCalculeazaTotal);
+            this.Controls.Add(this.btnAdaugaLinie);
+            this.Controls.Add(this.lvProduse);
+            this.Controls.Add(this.tbValoare);
+            this.Controls.Add(this.tbCantitate);
+            this.Controls.Add(this.tbPret);
+            this.Controls.Add(this.tbDenumire);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox tbDenumire;
+        private System.Windows.Forms.TextBox tbPret;
+        private System.Windows.Forms.TextBox tbCantitate;
+        private System.Windows.Forms.TextBox tbValoare;
+        private System.Windows.Forms.ListView lvProduse;
+        private System.Windows.Forms.Button btnAdaugaLinie;
+        private System.Windows.Forms.Button btnCalculeazaTotal;
+        private System.Windows.Forms.TextBox tbValoareTotala;
+        private System.Windows.Forms.Button btnComanda;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ Seminar7/Seminar7/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seminar7
+{
+    public partial class Form1 : Form
+    {
+        List<TextBox> listaTB = new List<TextBox>();
+        List<Produs> listaProduse = new List<Produs>();
+        public Form1()
+        {
+            InitializeComponent();
+            listaTB.Add(tbDenumire);
+            listaTB.Add(tbPret);
+            listaTB.Add(tbCantitate);
+            listaTB.Add(tbValoare);
+            tbCantitate.KeyPress += new KeyPressEventHandler(tbCifre_KeyPress);
+            tbPret.KeyPress += new KeyPressEventHandler(tbCifre_KeyPress);
+        }
+
+        //KeyPressEventArgs e:cotine datele evenimentului
+        //Clasa KeyPressEvent Args ofera informatii despre tasta apasata
+        private void tbCifre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //e.keyChar:Reprezinta caracterul tastei apasate
+            //propr keyChar din keyPressEventArgs este utilizata pt a det ce tasta a fost apasata
+            
+
+            //verifica daca dasta apasata este un nr intreg intre 0 si 9 sau tasta backspace(caracterul cu codul ASCII 8) sau cu.Enter
+            if(e.KeyChar>='0' && e.KeyChar<='9' || e.KeyChar==(char)Keys.Enter)
+            {
+                //daca conditia este adv,
+                //ev de apasare a tastei nu este bloct de manipulatorul de evenimente,
+                //permitand astfel caracterul sa fie introdus In TextBox
+                e.Handled = false; 
+            }
+            else
+            {
+                //previne introducerea caracterului in textBox prin trat evenimentului
+                //cand e pe true - eveniment procesat si nu mai propaga actiunea
+                e.Handled = true;
+            }
+        }
+
+        private void btnAdaugaLinie_Click(object sender, EventArgs e)
+        {
+            //obtine coordonata X a primului textBox din lista existenta
+            int X = listaTB[0].Location.X;
+
+            //obtine coordonata Y a ultimului TextBox din lista, pt a plasa noul TextBox dedesubtul acestuia
+            int Y = listaTB[listaTB.Count - 1].Location.Y;
+
+            //calc distata pe orizontala intre primul si al doilea textBox din lista
+            //pt a metine aceeasi distanta intre noile TextBox uri
+            int distantaTB = listaTB[1].Location.X - listaTB[0].Location.X;
+
+            //for care va adauga 4 noi TexBox
+            for(int i=0;i<4;i++)
+            {
+                TextBox tb = new TextBox(); //creaza un nou control textbox
+                tb.Location = new Point(X, Y + 40);// seteaza locatia noului tb, 40 pixeli dedesubtul utlimul tb din lista
+                X += distantaTB; //actualizeaza coordonata X pt urmatorul TextBox, bazat pe distanta calculata mai sus
+                tb.Width = tbDenumire.Width; //setaza latimea noului tb = cu tb specificat 
+
+                if(i==1 ||i==2)
+                {
+                    tb.KeyPress += new KeyPressEventHandler(tbCifre_KeyPress);
+                    //adauga manipulatorul de evenimente pt a restrictiona inputulu pt al doilea si al treilea tb adaugat
+                }
+                if (i==3)
+                {
+                    tb.ReadOnly = true;//seteaza al patrulea tb adaugat ca fiind doar pt citire
+                 
+                }
+
+                listaTB.Add(tb); //adauga noul tb in lista de textboxuri gesionate
+                this.Controls.Add(tb); //adauga noul tb la colectia de controale a formularului, pt a fi afisat pe ecran
+    
+            }
+        }
+
+        private void btnCalculeazaTotal_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear(); //curata orice erori prezentate anterior 
+            lvProduse.Items.Clear(); //elimina toate elementele din Lv, pregatindu l pt o noua lista de produse
+            double valoareTotala = 0; //init suma totala a valorilor produs
+
+            //parcurge lista de tb in pasi de 4, presup ca fiecare grup de 4 tb reprez un produs
+            for(int i=0;i<listaTB.Count;i+=4)
+            {
+                if (listaTB[i].Text == "")
+                    errorProvider1.SetError(listaTB[i], "Introduceti denumire!");
+                else if (listaTB[i + 1].Text == "")
+                    errorProvider1.SetError(listaTB[i + 1], "Introduceti pretul!");
+                else if (listaTB[i + 2].Text =="")
+                    errorProvider1.SetError(listaTB[i + 2], "Introduceti cantitatea!");
+                else
+                {
+                    string denumire = listaTB[i].Text;
+                    double pret = Convert.ToDouble(listaTB[i + 1].Text);
+                    int cantitate = Convert.ToInt32(listaTB[i + 2].Text);
+
+                    Produs p = new Produs(denumire, pret, cantitate);
+                    listaProduse.Add(p);
+
+                    double valoare = pret * cantitate;
+                    valoareTotala += valoare;
+                    listaTB[i + 3].Text = valoare.ToString();
+
+                    //creeaza un nou element ListView cu det produsului si il adauga in lv
+                    ListViewItem lvi = new ListViewItem(p.Denumire);
+                    lvi.SubItems.Add(p.Pret.ToString());
+                    lvi.SubItems.Add(p.Cantitate.ToString());
+                    lvi.SubItems.Add(valoare.ToString());
+                    lvProduse.Items.Add(lvi);
+                }
+
+                //actualizeaza textbox ul destinat afisarii val totale cu suma calculata
+                tbValoareTotala.Text = valoareTotala.ToString();
+            }
+        }
+
+        private void btnComanda_Click(object sender, EventArgs e)
+        {
+
+
+            string data = dateTimePicker1.Value.ToString("dd-MMMM-yyyy");
+            //MessageBox.Show(data);
+           
+            //TO DO
+            //SALVARE FISIER TEXT CU DENUMIRE Comanda_[data].txt
+            //Denumire | Cantitate | Pret | Valoare
+            //...
+            //Valoarea totala comanda: valoare totala
+        }
+
+     
+    }
+}
