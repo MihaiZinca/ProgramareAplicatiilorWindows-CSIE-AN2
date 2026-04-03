0eb107f28327ffbe94f1fb4a3bcb4f961a0f11e3
MihaiZinca <zincamihai24@stud.ase.ro>
2026-04-03 12:23:10 +0300

    +curs7

---
+++ Curs6/FirmaApp/FirmaApp/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirmaApp
+{
+    public partial class Form1 : Form
+    {
+        Firma obiectFirma;
+        public Form1()
+        {
+            InitializeComponent();
+            obiectFirma = new Firma("ASE_SRL"); //se apeleaza constructorul
+            obiectFirma.Ev_Modificare_Firma += ObiectFirma_Ev_Modificare_Firma; //PT A AFISA CE AM ADAUGAT
+        }
+        
+
+        void show_hide(bool val_bool)
+        {
+            //ne da popupuri   conversie pt a acesa subitems
+            ((ToolStripMenuItem)men.Items[0]).DropDownItems[1].Enabled = val_bool; //stocheaza
+            ((ToolStripMenuItem)men.Items[1]).DropDownItems[1].Enabled = val_bool; //modifca
+            ((ToolStripMenuItem)men.Items[1]).DropDownItems[2].Enabled = val_bool; //sterge
+        }
+        private void ObiectFirma_Ev_Modificare_Firma(object sender, Firma_EvArgs e)
+        {
+            Firma aux = sender as Firma; //sender este this
+            gv.Rows.Clear();//le eliminam pe toate
+            sbfs.Text = aux.Fond_Sal.ToString(); //fac legatura dintre model si form1 pt fond salarii sincronizare
+            foreach (Salariat s in aux.Salariati)
+                gv.Rows.Add(s.Marca.ToString(), s.Nume, s.Nr_ore.ToString(), s.Sal_ora.ToString());
+
+            if (aux.Numar_Salariati > 0)
+            {
+                gv.Rows[e.Index_Salariat].Selected = true;
+                show_hide(true);
+            }
+            else show_hide(false);
+            
+        }
+
+        private void iesireToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(DialogResult.Yes== MessageBox.Show("Esti sigur ?", "DaNu", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+            {
+                Application.Exit();
+            }
+        }
+
+        private void adaugaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form_actualizare fa = new Form_actualizare();
+            string soptiune = ((ToolStripMenuItem)sender).Tag.ToString();
+            if (soptiune == "A")
+            {
+                fa.Text = "Adaugare Salariat";
+                fa.button1.Text = "Adauga";
+            }
+            else
+            {
+                fa.Text = "Modificare Salariat";
+                fa.button1.Text = "Modifica";
+                DataGridViewRow rd = gv.SelectedRows[0];
+                fa.tbMa.Text = rd.Cells[0].Value.ToString();
+                fa.tbNp.Text=rd.Cells[1].Value.ToString();
+                fa.tbNo.Text= rd.Cells[2].Value.ToString();
+                fa.tbSo.Text=rd.Cells[3].Value.ToString();
+
+
+            }
+            if(DialogResult.OK==fa.ShowDialog()) //daca apas pe Adauga se construieste....
+            {
+                Salariat sal = new Salariat
+                {
+                    Marca = int.Parse(fa.tbMa.Text),
+                    Nume = fa.tbNp.Text,
+                    Nr_ore = int.Parse(fa.tbNo.Text),
+                    Sal_ora = int.Parse(fa.tbSo.Text)
+                };
+                if (soptiune == "A") obiectFirma.Adauga_Salariat(sal);
+                else
+                    obiectFirma[gv.SelectedRows[0].Index] = sal;
+            }    
+
+            //ma folosesc de TAG(properties) ca sa stiu de pe ce buton a apasat! daca pe adauga/modifica
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text += obiectFirma.Nume_Firma; //mai intai se apelaza constructorul si dupa in load se foloseste
+            show_hide(false);
+        }
+
+        private void deschideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd=new OpenFileDialog();
+            ofd.InitialDirectory = "C:\\";
+            ofd.Filter = "Fisiere de date|*.dat|Toate fisierele|*.*";
+            if (DialogResult.OK == ofd.ShowDialog())
+                obiectFirma.Deserializeaza(ofd.FileName);
+
+           // obiectFirma.Deserializeaza("fsal.dat"); nu l mai dau asa, ci sa l prelucrez de undeva
+        }
+
+        private void stocheazaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = "C:\\";
+            sfd.Filter = "Fisiere de date|*.dat|Toate fisierele|*.*";
+
+            if (DialogResult.OK == sfd.ShowDialog())
+            {
+                obiectFirma.Serializeaza(sfd.FileName);
+            }
+
+            //obiectFirma.Serializeaza("fsal.dat");
+
+        }
+    }
+    //serializare -salvarea datelor intr un fisier
+    //deserializarea -restaurarea datelor intr un fisier
+    //in prima etapa trebuie sa definesc starea obiectului!! 
+
+    
+    
+}
---
+++ Curs6/FirmaApp/FirmaApp/Model.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Net.Http.Headers;
+using System.IO; //pt serializare 
+
+namespace FirmaApp
+{
+
+    public class Firma_EvArgs : EventArgs
+    {
+
+        int indexMat;
+        public Firma_EvArgs(int _indexMat)
+        {
+            indexMat = _indexMat;
+        }
+        public int Index_Salariat => indexMat;
+    }
+
+    [DataContract]
+    //vom defini doar proprietatile
+    public class Salariat
+    {
+        [DataMember]public int Marca {  get; set; }  //ascundem atributele, nu lucram direct cu ele, ci cu proprietatea
+        [DataMember] public string Nume { get; set; }
+        [DataMember] public int Nr_ore  { get; set; }
+
+        [DataMember] public int Sal_ora { get; set; }
+
+        public int Salariul => Nr_ore * Sal_ora; // => proprietate de tip read only
+    }
+
+    [DataContract]
+    public class Firma
+    {
+        //facem class Firma_EvArgs
+        public event EventHandler<Firma_EvArgs> Ev_Modificare_Firma; //ori de cate ori cineva imi modifica oviectul Firma
+        [DataMember] string numeFirma;
+        [DataMember] List<Salariat> ls = null;
+
+        public Firma(string _numeFirma="Firma.SRL")
+        {
+            numeFirma = _numeFirma;
+            ls = new List<Salariat>();
+        }
+
+        public List<Salariat> Salariati => ls; //returnam lista 
+        public int  Numar_Salariati => ls.Count;
+
+        public string Nume_Firma => numeFirma;
+
+        public int Fond_Sal => ls.Sum(s => s.Salariul);
+        
+        public void Adauga_Salariat(Salariat obs)
+        {
+            //pt principiu cu cheie unica - sa nu fie cheie prima dublicata, si sa nu fie nulla -to do!
+            //Salariat aux=ls.Find(s => s.Marca == obs.Marca);
+            //if (aux != null) throw new Exception("Marca Dublicata!!!!");
+
+            ls.Add(obs);
+            Ev_Modificare_Firma?.Invoke(this, new Firma_EvArgs(ls.Count - 1)); //?.- daca nu e null this=sender ,
+        }
+
+        public Salariat this[int k]
+        {
+            get => ls[k];
+            set
+            {
+                if (ls[k]!=value)
+                {
+                    ls[k] = value;
+                    Ev_Modificare_Firma?.Invoke(this, new Firma_EvArgs(k));
+                }
+            }
+        }
+
+        //2 variante - pt gestioanre
+        //1-principiu baze de date, de a gestiona marca ca fiind o cheie unica, si gestionare dupa marca, 
+        //2-de a opera cu salariatul din firma pe baza indexului pe care il procesez la un mom dat(!asta facem)
+
+        //tipul delegatului din windows forms - sender si
+        //EventArgs-tine informatie care se impacheteaza odt cu ridicarea evenimentului pt a avea informatii mai detaliate
+
+        //serializare -salvarea datelor intr un fisier
+        //deserializarea -restaurarea datelor intr un fisier
+        //in prima etapa trebuie sa definesc starea obiectului!! 
+
+        public void Serializeaza(string numeFisier)
+        {
+            FileStream fs = new FileStream(numeFisier, FileMode.Create); //creez fisierul
+            DataContractSerializer obs = new DataContractSerializer(typeof(Firma));
+            obs.WriteObject(fs, this); //this este vorba de obiectul firma
+        }
+
+        public void Deserializeaza(string numeFisier)
+        {
+            FileStream fs = new FileStream(numeFisier, FileMode.Open);
+            DataContractSerializer obs = new DataContractSerializer(typeof(Firma));
+            Firma aux=obs.ReadObject(fs) as Firma; // as Firma tot cast
+
+            numeFirma = aux.numeFirma;
+            ls.Clear(); //elimin toti sal din firma
+                        //si ii iau pe aia din aux si pun la aceasta firma
+            foreach (Salariat s in aux.Salariati)
+                Adauga_Salariat(s);
+
+            fs.Close();
+        }
+
+    }
+
+}
+
