b2dac3fe7666867822bfd6094a184ccf83ed82bf
Zinca Mihai-Cristian <zincamihai24@stud.ase.ro>
2026-04-08 22:26:31 +0300

    Add files via upload

---
+++ lucru/FirmaApp/FirmaApp/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirmaApp
+{
+    public partial class Form1 : Form
+    {
+        Firma obiectFirma;
+        public Form1()
+        {
+            InitializeComponent();
+            obiectFirma = new Firma("ASE_SRL");
+            obiectFirma.Ev_Modificare_Firma += ObiectFirma_Ev_Modificare_Firma;
+        }
+
+        private void ObiectFirma_Ev_Modificare_Firma(object sender, Firma_EvArgs e)
+        {
+            Firma aux = sender as Firma;
+            gv.Rows.Clear();
+            foreach(Salariat s in aux.Salariati)
+                gv.Rows.Add(s.Marca.ToString(),s.Nume,s.Nr_ore.ToString(),s.Sal_ora.ToString());
+            if (aux.Numar_Salariati > 0)
+                gv.Rows[e.Index_Salariat].Selected = true;
+        }
+
+        private void iesireToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(DialogResult.Yes== MessageBox.Show("Esti sigur ?", "DaNu", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+            {
+                Application.Exit();
+            }
+        }
+
+        private void adaugaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form_actualizare fa = new Form_actualizare();
+            string soptiune = ((ToolStripMenuItem)sender).Tag.ToString();
+            if (soptiune == "A")
+            {
+                fa.Text = "Adaugare Salariat";
+                fa.button1.Text = "Adauga";
+            }
+            else
+            {
+                fa.Text = "Modificare Salariat";
+                fa.button1.Text = "Modifica";
+                DataGridViewRow rd = gv.SelectedRows[0];
+                fa.tbMa.Text = rd.Cells[0].Value.ToString();
+                fa.tbNp.Text = rd.Cells[1].Value.ToString();
+                fa.tbNo.Text=rd.Cells[2].Value.ToString();
+                fa.tbSo.Text=rd.Cells[3].Value.ToString();
+            }
+            if (DialogResult.OK==fa.ShowDialog())
+            {
+                Salariat sal = new Salariat
+                {
+                    Marca = int.Parse(fa.tbMa.Text),
+                    Nume = fa.tbNp.Text,
+                    Nr_ore = int.Parse(fa.tbNo.Text),
+                    Sal_ora = int.Parse(fa.tbSo.Text)
+                };
+
+                if (soptiune == "A") 
+                    obiectFirma.Adauga_Salariat(sal);
+                else
+                    obiectFirma[gv.SelectedRows[0].Index] = sal;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text += obiectFirma.Nume_Firma;
+        }
+    }
+}
---
+++ lucru/FirmaApp/FirmaApp/Model.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirmaApp
+{
+
+    public class Firma_EvArgs : EventArgs
+    {
+        int indexMat;
+        public Firma_EvArgs(int indexMat)
+        {
+            this.indexMat = indexMat;
+        }
+        public int Index_Salariat => indexMat;
+    }
+
+    public class Salariat
+    {
+        public int Marca {  get; set; }
+        public string Nume {  get; set; }
+        public int Nr_ore {  get; set; }
+        public int Sal_ora {  get; set; }
+
+        public int Salariul => Nr_ore * Sal_ora;
+
+    }
+
+    public class Firma
+    {
+        public event EventHandler<Firma_EvArgs> Ev_Modificare_Firma;
+        string numeFirma;
+        List<Salariat> listaSal = null;
+
+        public Firma(string _numeFirma="Firma.SRL")
+        {
+            numeFirma = _numeFirma; 
+            listaSal=new List<Salariat>();
+        }
+        public List<Salariat> Salariati => listaSal;
+        public int Numar_Salariati => listaSal.Count;
+        public string Nume_Firma => numeFirma;
+
+        public int Fond_Sal => listaSal.Sum(s => s.Salariul);
+
+        public void Adauga_Salariat(Salariat obs)
+        {
+            listaSal.Add(obs);
+            Ev_Modificare_Firma?.Invoke(this, new Firma_EvArgs(listaSal.Count - 1)); 
+
+        }
+
+        public Salariat this[int k]
+        {
+            get => listaSal[k];
+            set
+            {
+                if (listaSal[k]!=value)
+                {
+                    listaSal[k] = value;
+                    Ev_Modificare_Firma?.Invoke(this, new Firma_EvArgs(k));
+
+                }
+            }
+        }
+    }
+}
