a20c34509a654c6ca18c2afadc4b288136861973
Zinca Mihai-Cristian <zincamihai24@stud.ase.ro>
2026-05-14 15:41:12 +0300

    Add files via upload

---
+++ Curs12/Curs12/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Curs12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gv = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnStergere = new System.Windows.Forms.Button();
+            this.tbMarca = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gv
+            // 
+            this.gv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gv.Location = new System.Drawing.Point(56, 81);
+            this.gv.Name = "gv";
+            this.gv.RowHeadersWidth = 62;
+            this.gv.RowTemplate.Height = 28;
+            this.gv.Size = new System.Drawing.Size(503, 276);
+            this.gv.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(631, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStergere
+            // 
+            this.btnStergere.Location = new System.Drawing.Point(631, 310);
+            this.btnStergere.Name = "btnStergere";
+            this.btnStergere.Size = new System.Drawing.Size(121, 70);
+            this.btnStergere.TabIndex = 2;
+            this.btnStergere.Text = "Stergere tuplu";
+            this.btnStergere.UseVisualStyleBackColor = true;
+            this.btnStergere.Click += new System.EventHandler(this.btnStergere_Click);
+            // 
+            // tbMarca
+            // 
+            this.tbMarca.Location = new System.Drawing.Point(631, 266);
+            this.tbMarca.Name = "tbMarca";
+            this.tbMarca.Size = new System.Drawing.Size(133, 26);
+            this.tbMarca.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbMarca);
+            this.Controls.Add(this.btnStergere);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gv;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnStergere;
+        private System.Windows.Forms.TextBox tbMarca;
+    }
+}
+
---
+++ Curs12/Curs12/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Curs12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string scmd = "select * from persoane";
+            con.Open();
+            SqLConmand obcnd = new SqlCommand(scmd, con);
+            SqlDataReader dr = obcmd.ExecuteReader(); 
+            dt.Load(dr);
+            gv.DataSource = dt;
+            con.CloseO;
+
+            //partea de sus pusa intr o functie separata de vizualizare
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sm = "700", sn = "Valentina";
+            string ins_cmd = "insert into persoane values(";
+            ins_cmd+=sm+","+sn++"')";
+            con.Opne();
+            SqLConmand obcmd = new SqlCommand(ins_cmd, con);
+            try
+            {
+                int k = obcmd.ExecuteNonQuery();
+                MessageBox.Show("Inserat: " + k.ToString() + " tupluri");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnStergere_Click(object sender, EventArgs e)
+        {
+            string del_cmd = "delete from persoane where marca =@marca";
+            con.Opne();
+            SqLConmand obcmd = new SqlCommand(del_cmd, con);
+            obcmd.Parameters.Add("@marca", SqlDbType.Int).Value = int.Parse(tbMarca.Text);
+            try
+            {
+                int k = obcmd.ExecuteNonQuery();
+                MessageBox.Show("Sters: " + k.ToString() + " tupluri");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+    }
+}
