6835be5cf5ded40431c417b66d64f25e5e235e40
Zinca Mihai-Cristian <zincamihai24@stud.ase.ro>
2026-05-29 19:22:30 +0300

    Add files via upload

---
+++ Practice Exam/SubPaw2025/GestiuneAngajati/Companie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestiuneAngajati
+{
+    public class Companie
+    {
+        private int id;
+        private string nume;
+
+        public Companie()
+        {
+        }
+
+        public Companie(int id, string nume)
+        {
+            this.id = id;
+            this.nume = nume;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nume { get => nume; set => nume = value; }
+    }
+}
---
+++ Practice Exam/SubPaw2025/GestiuneAngajati/FormAdaugare.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace GestiuneAngajati
+{
+    partial class FormAdaugare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAdaugare = new System.Windows.Forms.Button();
+            this.tbNume = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbIdCompanie = new System.Windows.Forms.TextBox();
+            this.btnSalvare = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.dtN = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdaugare
+            // 
+            this.btnAdaugare.Location = new System.Drawing.Point(56, 215);
+            this.btnAdaugare.Name = "btnAdaugare";
+            this.btnAdaugare.Size = new System.Drawing.Size(134, 37);
+            this.btnAdaugare.TabIndex = 13;
+            this.btnAdaugare.Text = "Adaugare";
+            this.btnAdaugare.UseVisualStyleBackColor = true;
+            this.btnAdaugare.Click += new System.EventHandler(this.btnAdaugare_Click);
+            // 
+            // tbNume
+            // 
+            this.tbNume.Location = new System.Drawing.Point(177, 53);
+            this.tbNume.Name = "tbNume";
+            this.tbNume.Size = new System.Drawing.Size(154, 22);
+            this.tbNume.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Id Companie";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Data nasterii";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nume";
+            // 
+            // tbIdCompanie
+            // 
+            this.tbIdCompanie.Location = new System.Drawing.Point(177, 152);
+            this.tbIdCompanie.Name = "tbIdCompanie";
+            this.tbIdCompanie.Size = new System.Drawing.Size(154, 22);
+            this.tbIdCompanie.TabIndex = 15;
+            // 
+            // btnSalvare
+            // 
+            this.btnSalvare.Location = new System.Drawing.Point(219, 217);
+            this.btnSalvare.Name = "btnSalvare";
+            this.btnSalvare.Size = new System.Drawing.Size(112, 35);
+            this.btnSalvare.TabIndex = 16;
+            this.btnSalvare.Text = "Salveaza";
+            this.btnSalvare.UseVisualStyleBackColor = true;
+            this.btnSalvare.Click += new System.EventHandler(this.btnSalvare_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // dtN
+            // 
+            this.dtN.Location = new System.Drawing.Point(177, 98);
+            this.dtN.Name = "dtN";
+            this.dtN.Size = new System.Drawing.Size(200, 22);
+            this.dtN.TabIndex = 17;
+            // 
+            // FormAdaugare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 313);
+            this.Controls.Add(this.dtN);
+            this.Controls.Add(this.btnSalvare);
+            this.Controls.Add(this.tbIdCompanie);
+            this.Controls.Add(this.btnAdaugare);
+            this.Controls.Add(this.tbNume);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormAdaugare";
+            this.Text = "FormAdaugare";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdaugare;
+        private System.Windows.Forms.TextBox tbNume;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbIdCompanie;
+        private System.Windows.Forms.Button btnSalvare;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.DateTimePicker dtN;
+    }
+}
